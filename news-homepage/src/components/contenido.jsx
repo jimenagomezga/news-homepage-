2eaf99ad7722c6daf4784490ab4096cbd8f662f3
jimenagomezga <jimenagomezga@gmail.com>
2024-03-24 20:22:29 -0500

    version escritorio final

--- news-homepage/src/components/contenido.jsx
+++ news-homepage/src/components/contenido.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import '.././styles/contenido.css';
 import { Contentvertical } from '../components/contentvertical'
-
-
-import Banner from '../images/image-web-3-desktop.jpg';
+import { Contenido1 } from '../components/contenido1';
+// import Banner from '../images/image-web-3-desktop.jpg';
 
 
 export const Contenido = () => {
     return (
         <div className="container1">
-            <img src={Banner} alt="banner" />
+            <Contenido1 />
+            {/* <img src={Banner} alt="banner" /> */}
             <Contentvertical />
 
         </div>
---
+++ news-homepage/src/components/contenido1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Banner from '../images/image-web-3-desktop.jpg';
+import '.././styles/contenido1.css';
+
+export const Contenido1 = () => {
+    return (
+        <div>
+            <img src={Banner} alt="banner" />
+            <div className="container2">
+                <h1>The Bright Future of Web 3.0?</h1>
+                <div className="container3">
+                    <p>We dive into the next evolution of web that calms to put the power of the platform back into the hands of the people. But is it really fulfilling its promise?</p>
+                    <button>READ MORE</button>
+                </div>
+
+            </div >
+        </div>
+    )
+}
